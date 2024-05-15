f42708fa2603eb8e6171e073c502386a3f423db2
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-05-15 22:06:23 +0100

    Remove temporary design that was mistakenly commited

--- src/components/TripProp.tsx
+++ src/components/TripProp.tsx
@@ -16,16 +16,16 @@ export default function TripProp({
 }: Readonly<TripPropProps>) {
   return (
     <div className="flex flex-col gap-2 items-center">
-      <div className={"flex flex-row gap-1 items-center"}>
-        <MaterialSymbol icon={icon} size={20} className="text-default-500" />
-        <p className="text-default-500">{title}</p>
-      </div>
       <div className="flex items-center h-8">
         {isText ? (
           <p className="text-2xl font-semibold">{children}</p>
         ) : (
           children
         )}
+      </div>
+      <div className={"flex flex-row gap-1 items-center"}>
+        <MaterialSymbol icon={icon} size={20} className="text-default-500" />
+        <p className="text-default-500 text-lg">{title}</p>
       </div>
     </div>
   );
