1e093ad3e539469419d93e34a3efd53312f1e63d
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-05-15 16:18:28 +0100

    Add 3-col grid in xl breakpoint

--- src/components/TripGroup.tsx
+++ src/components/TripGroup.tsx
@@ -6,13 +6,13 @@ interface TripGroupProps {
   type: "departure" | "arrival";
 }
 
-export default function TripGroup({ trips, type }: TripGroupProps) {
+export default function TripGroup({ trips, type }: Readonly<TripGroupProps>) {
   return (
     <div className="flex-1">
       <p className="text-4xl font-bold text-center mb-8">
         {type === "departure" ? "Departures" : "Arrivals"}
       </p>
-      <div className="grid gap-8 grid-cols-1 lg:grid-cols-2">
+      <div className="grid gap-8 grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
         {trips.map((trip) => (
           <TripCard key={trip.id} trip={trip} type={type} />
         ))}
