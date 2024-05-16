5354d0013b6f8d4e1d01f7140a2f25f1786ffa88
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-05-16 01:49:25 +0100

    Add websocket connection to trips

--- src/components/TripGroup.tsx
+++ src/components/TripGroup.tsx
@@ -1,12 +1,18 @@
 import { Trip } from "@/types/trip";
 import TripCard from "./TripCard";
 import { Config } from "@/types/config.ts";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getTrips } from "@/services/tripService.ts";
 import { CircularProgress } from "@nextui-org/react";
+import { useEffect } from "react";
+import { BASE_API_URL } from "@/services/config.ts";
+import SockJS from "sockjs-client";
+import Stomp from "stompjs";
 
 export default function TripGroup() {
   const config = JSON.parse(localStorage.getItem("config")!) as Config;
+
+  const queryClient = useQueryClient();
 
   const { data, isPending } = useQuery<Trip[]>({
     queryKey: ["trips", config.city, config.type],
@@ -18,6 +24,38 @@ export default function TripGroup() {
       ).then((res) => res.slice(0, 6)),
     initialData: [],
   });
+
+  useEffect(() => {
+    const ws = new SockJS(`${BASE_API_URL}ws`);
+    const client = Stomp.over(ws);
+
+    client.connect(
+      {},
+      () => {
+        client.subscribe(
+          `/signage/cities/${config.city}/${config.type}`,
+          (new_data) => {
+            const parsedData = JSON.parse(new_data.body) as Trip[];
+            queryClient.setQueryData(
+              ["trips", config.city, config.type],
+              () => {
+                return parsedData.map((trip) => {
+                  return {
+                    ...trip,
+                    departureTime: new Date(trip.departureTime),
+                    arrivalTime: new Date(trip.arrivalTime),
+                  };
+                });
+              },
+            );
+          },
+        );
+      },
+      (e) => {
+        console.error("WebSocket connection failed", e);
+      },
+    );
+  }, [config.city, config.type, queryClient]);
 
   return (
     <div className="flex-1">
