eec625b3f6744f1c5cdd52ee701b455491a82282
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-05-22 17:22:09 +0100

    Update the API path

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,3 +1,3 @@
 const VITE_HOST_URL = import.meta.env.VITE_HOST_URL as string;
-export const BASE_API_URL = `http://api.${VITE_HOST_URL}/api/public/`;
-export const WS_API_URL = `ws://api.${VITE_HOST_URL}/api/public/ws`;
+export const BASE_API_URL = `http://${VITE_HOST_URL}/api/public/`;
+export const WS_API_URL = `ws://${VITE_HOST_URL}/api/public/ws`;
